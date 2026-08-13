5fda55976c42c14c5db881527248c62eb38ad5f6
ChenZhao <2381401246@qq.com>
2026-08-14 00:16:20 +0100

    standardize files

--- format_unifier.py
+++ format_unifier.py
@@ -1,8 +1,6 @@
 import json
 import os
 
-
-
 def format_standardized():
     input_folder = "savefile"
     output_folder = "savefile/standardized_file"
@@ -10,7 +8,10 @@ def format_standardized():
 
     for name in os.listdir(input_folder):
         if name.endswith(".json"):
-            with open(input_folder + "/" + name) as file:
-                data = json.load(file)
+            with open(input_folder + "/" + name, "r") as file:
+                data = file.read()
 
+            data = data.lower().replace("_", " ")
 
+            with open(output_folder + "/" + name, "w") as file:
+                file.write(data)
