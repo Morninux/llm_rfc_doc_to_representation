e1655d64580195459e01b1a147508a4913450e56
ChenZhao <2381401246@qq.com>
2026-08-15 23:53:19 +0100

    add average normalized levenshtein similarity function

--- evaluator.py
+++ evaluator.py
@@ -1,9 +1,26 @@
 import json
+import os
+import Levenshtein
+import nltk.translate.bleu_score
 
-def load_json(path):
-    with open(path, "r", encoding="utf-8") as file:
-        return json.load(file)
+def ANLS(reference, standardized_test_file):
+    with open(reference, "r", encoding="utf-8") as file:
+        rf = json.load(file) #reference file
 
-def normalize_rfc_number(value):
-    number = str(value)
-    return number
+    nls_list = [] #list of normalized levenshtein similarities
+    for name in os.listdir(standardized_test_file):
+        if not name.endswith(".json"):
+            continue
+
+        with open(standardized_test_file + "/" + name, "r", encoding="utf-8") as file:
+            stf = json.load(file) #standardized test file
+
+        #nomalized_levenshtein_similarity = 1 - Levenshtein_Distance(doc_test, doc_real) / max(len(doc_test), len(doc_real))
+        nls = 1 - (Levenshtein.distance(rf, stf) / max(len(rf), len(stf)))
+        nls_list.append(nls)
+        print(name, nls)
+
+    anls = sum(nls_list) / len(nls_list) #average normalized levenshtein similarity
+    return anls
+
+
